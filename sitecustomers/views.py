355ea836d89d7cb67cd31dfa973549ab9c442d7e
Naman Nimbale <67785931+n1a9m9a6n@users.noreply.github.com>
2021-03-03 23:12:49 +0530

    Add files via upload

---
+++ sitecustomers/views.py
@@ -0,0 +1,208 @@
+from django.shortcuts import render,redirect,reverse
+from ecom import forms,models
+from django.http import HttpResponseRedirect,HttpResponse
+from django.core.mail import send_mail
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib import messages
+from django.conf import settings
+
+# Create your views here
+
+#---------------------------------------------------------------------------------
+#------------------------ CUSTOMER RELATED VIEWS ------------------------------
+#---------------------------------------------------------------------------------
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def customer_home_view(request):
+    products=models.Product.objects.all()
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart=0
+    return render(request,'customer_home.html',{'products':products,'product_count_in_cart':product_count_in_cart})
+
+
+
+# shipment address before placing order
+@login_required(login_url='customerlogin')
+def customer_address_view(request):
+    # this is for checking whether product is present in cart or not
+    # if there is no product in cart we will not show address form
+    product_in_cart=False
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        if product_ids != "":
+            product_in_cart=True
+    #for counter in cart
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart=0
+
+    addressForm = forms.AddressForm()
+    if request.method == 'POST':
+        addressForm = forms.AddressForm(request.POST)
+        if addressForm.is_valid():
+            # here we are taking address, email, mobile at time of order placement
+            # we are not taking it from customer account table because
+            # these thing can be changes
+            email = addressForm.cleaned_data['Email']
+            mobile=addressForm.cleaned_data['Mobile']
+            address = addressForm.cleaned_data['Address']
+            #for showing total price on payment page.....accessing id from cookies then fetching  price of product from db
+            total=0
+            if 'product_ids' in request.COOKIES:
+                product_ids = request.COOKIES['product_ids']
+                if product_ids != "":
+                    product_id_in_cart=product_ids.split('|')
+                    products=models.Product.objects.all().filter(id__in = product_id_in_cart)
+                    for p in products:
+                        total=total+p.price
+
+            response = render(request, 'payment.html',{'total':total})
+            response.set_cookie('email',email)
+            response.set_cookie('mobile',mobile)
+            response.set_cookie('address',address)
+            return response
+    return render(request,'customer_address.html',{'addressForm':addressForm,'product_in_cart':product_in_cart,'product_count_in_cart':product_count_in_cart})
+
+
+
+
+# here we are just directing to this view...actually we have to check whther payment is successful or not
+#then only this view should be accessed
+@login_required(login_url='customerlogin')
+def payment_success_view(request):
+    # Here we will place order | after successful payment
+    # we will fetch customer  mobile, address, Email
+    # we will fetch product id from cookies then respective details from db
+    # then we will create order objects and store in db
+    # after that we will delete cookies because after order placed...cart should be empty
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    products=None
+    email=None
+    mobile=None
+    address=None
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        if product_ids != "":
+            product_id_in_cart=product_ids.split('|')
+            products=models.Product.objects.all().filter(id__in = product_id_in_cart)
+            # Here we get products list that will be ordered by one customer at a time
+
+    # these things can be change so accessing at the time of order...
+    if 'email' in request.COOKIES:
+        email=request.COOKIES['email']
+    if 'mobile' in request.COOKIES:
+        mobile=request.COOKIES['mobile']
+    if 'address' in request.COOKIES:
+        address=request.COOKIES['address']
+
+    # here we are placing number of orders as much there is a products
+    # suppose if we have 5 items in cart and we place order....so 5 rows will be created in orders table
+    # there will be lot of redundant data in orders table...but its become more complicated if we normalize it
+    for product in products:
+        models.Orders.objects.get_or_create(customer=customer,product=product,status='Pending',email=email,mobile=mobile,address=address)
+
+    # after order placed cookies should be deleted
+    response = render(request,'payment_success.html')
+    response.delete_cookie('product_ids')
+    response.delete_cookie('email')
+    response.delete_cookie('mobile')
+    response.delete_cookie('address')
+    return response
+
+
+
+
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def my_order_view(request):
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    orders=models.Orders.objects.all().filter(customer_id = customer)
+    ordered_products=[]
+    for order in orders:
+        ordered_product=models.Product.objects.all().filter(id=order.product.id)
+        ordered_products.append(ordered_product)
+
+    return render(request,'my_order.html',{'data':zip(ordered_products,orders)})
+
+
+
+
+#--------------for discharge patient bill (pdf) download and printing
+import io
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = io.BytesIO()
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return
+
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def download_invoice_view(request,orderID,productID):
+    order=models.Orders.objects.get(id=orderID)
+    product=models.Product.objects.get(id=productID)
+    mydict={
+        'orderDate':order.order_date,
+        'customerName':request.user,
+        'customerEmail':order.email,
+        'customerMobile':order.mobile,
+        'shipmentAddress':order.address,
+        'orderStatus':order.status,
+
+        'productName':product.name,
+        'productImage':product.product_image,
+        'productPrice':product.price,
+        'productDescription':product.description,
+
+
+    }
+    return render_to_pdf('download_invoice.html',mydict)
+
+
+
+
+
+
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def my_profile_view(request):
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    return render(request,'my_profile.html',{'customer':customer})
+
+
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def edit_profile_view(request):
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    user=models.User.objects.get(id=customer.user_id)
+    userForm=forms.CustomerUserForm(instance=user)
+    customerForm=forms.CustomerForm(request.FILES,instance=customer)
+    mydict={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST,instance=user)
+        customerForm=forms.CustomerForm(request.POST,instance=customer)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customerForm.save()
+            return HttpResponseRedirect('my-profile')
+    return render(request,'edit_profile.html',context=mydict)
+
+
